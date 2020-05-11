c7c9310629df510fb85b60051933589a84714ace
ontario1876 <ko.nakamura.10.13@gmail.com>
2020-05-12 07:12:15 +0900

    feat(firebase) : firebaseとangularを連携 #11

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyClRRWqDU2hQ10rfC16nzrM5qLd3VnsDNA',
+    authDomain: 'fil-portal.firebaseapp.com',
+    databaseURL: 'https://fil-portal.firebaseio.com',
+    projectId: 'fil-portal',
+    storageBucket: 'fil-portal.appspot.com',
+    messagingSenderId: '667624410306',
+    appId: '1:667624410306:web:ed5f8b0d4e34177b1b3f5b',
+    measurementId: 'G-5WKHMDDCY7'
+  }
 };
 
 /*
